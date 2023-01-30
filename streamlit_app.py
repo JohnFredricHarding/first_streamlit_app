b8d5a0a68a7227284b8cd4a442f95e9cdebaeafc
JohnFredricHarding <123699384+JohnFredricHarding@users.noreply.github.com>
2023-01-30 14:46:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit choice)
-#Next row just lists the information from the response as one long string ie Just writes data to the screen
-streamlit.text(fruityvice_response.json())# Just writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit choice)
 
 #Take JSON version of response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
