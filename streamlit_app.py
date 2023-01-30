9023746a9bc7eca606df99592ac92d47fe7d2fc2
JohnFredricHarding <123699384+JohnFredricHarding@users.noreply.github.com>
2023-01-30 14:42:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit choice)
 #Next row just lists the information from the response as one long string ie Just writes data to the screen
 streamlit.text(fruityvice_response.json())# Just writes data to the screen
 
